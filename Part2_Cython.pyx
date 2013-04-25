650fdb7a8ee95cf7cbc8febaa6e74680548829b2
wtkleonard <wongting@uw.edu>
2013-04-24 20:42:39 -0700

    Update Part2_Cython.pyx

--- Part2_Cython.pyx
+++ Part2_Cython.pyx
@@ -2,10 +2,10 @@
 
 %cython
 
-def cython_sum_to_n(int n):
+def cython_sum_sq(int n):
     cdef long S = 0
     for i in range(1, n+1):
-        S = S + i
+        S = S + i*i
     return(S)
 
-timeit("cython_sum_to_n(10000)")
+timeit("cython_sum_sq(10000)")
