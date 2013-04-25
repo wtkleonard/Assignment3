13bf71176db6b2c35a0597a93a04627e4d459b64
wtkleonard <wongting@uw.edu>
2013-04-24 20:42:03 -0700

    Update Part2_Python.py

--- Part2_Python.py
+++ Part2_Python.py
@@ -1,14 +1,11 @@
-
-# Compute the sum of the squares of
-# the positive integers up to n, where
-# you may assume that n is at most 10000.
+# Part 2
 
 # Implementation using Python
-def sum_to_n(n):
+
+def sum_sq(n):
     S = 0
     for i in range(1, n+1):
-        S = S + i
+        S = S + i*i
     return(S)
-
-# Time the implementation
-timeit("sum_to_n(10000)")
+    
+timeit("sum_sq(10000)")
